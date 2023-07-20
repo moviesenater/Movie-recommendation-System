837b40159551b425fdab34de9631f055b50017d5
moviesenater <139667683+moviesenater@users.noreply.github.com>
2023-07-20 08:59:23 -0700

    Add files via upload

--- http.java
+++ http.java
@@ -1,112 +1,63 @@
-package org.java;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
 
-public class http{
-    private static final int MAX_MOVIES_PER_GENRE = 10;
-    private static Map<String, List<JCheckBox>> selectedMoviesPerGenre = new HashMap<>();
-    private static JFrame frame;
-    private static JPanel movieListPanel;
-
+public class http {
     public static void main(String[] args) throws Exception {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
-
-    public static void createAndShowGUI() {
-        frame = new JFrame("Movie Selection");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-
-        // Input Panel
-        JPanel inputPanel = new JPanel(new GridLayout(2, 2, 10, 10));
-        JTextField actionField = new JTextField();
-        JTextField langField = new JTextField();
-        inputPanel.add(new JLabel("Action Genre:"));
-        inputPanel.add(actionField);
-        inputPanel.add(new JLabel("Language:"));
-        inputPanel.add(langField);
-
-        // Button to Fetch Movies
-        JButton fetchButton = new JButton("Fetch Movies");
-        fetchButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    String action = actionField.getText();
-                    String lang = langField.getText();
-                    fetchMovies(action, lang);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-
-        // Movie List Panel
-        movieListPanel = new JPanel(new GridLayout(0, 1, 10, 10));
-
-        // Add components to the frame
-        frame.add(inputPanel, BorderLayout.NORTH);
-        frame.add(fetchButton, BorderLayout.CENTER);
-        frame.add(movieListPanel, BorderLayout.SOUTH);
-
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public static void fetchMovies(String action, String lang) throws Exception {
-        String apiUrl = "https://ott-details.p.rapidapi.com/advancedsearch?start_year=2020&end_year=2020&min_imdb=6&max_imdb=7.8&genre=" + action + "&language=" + lang + "&type=movie&sort=latest&page=1";
-        HttpRequest request = HttpRequest.newBuilder(new URI(apiUrl))
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter genre: ");
+        String genre = s.nextLine();
+        System.out.print("Enter language: ");
+        String lang = s.next();
+        s.close();
+        try{
+            HttpRequest request = HttpRequest.newBuilder(new URI("https://ott-details.p.rapidapi.com/advancedsearch?start_year=2020&end_year=2020&min_imdb=6&max_imdb=7.8&genre=" + genre + "&language=" + lang + "&type=movie&sort=latest&page=1"))
                 .header("X-RapidAPI-Key", "65fd17147fmshc51ddabff33471cp1f7ccfjsn59de4e138bd0")
                 .header("X-RapidAPI-Host", "ott-details.p.rapidapi.com")
                 .build();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String responseBody = response.body();
-
-        JSONParser parser = new JSONParser();
-        JSONObject json = (JSONObject) parser.parse(responseBody);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            String responseBody = response.body(); 
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(responseBody);
 
         // Access the data from the JSON object
-        JSONArray results = (JSONArray) json.get("results");
-
-        // Clear the movie list panel before adding new movies
-        movieListPanel.removeAll();
-        selectedMoviesPerGenre.put(action, new ArrayList<>());
+            JSONArray results = (JSONArray) json.get("results");
 
         // Iterate over the results array and access individual objects
-        int count = 0;
-        for (int i = 0; i < results.size() && count < MAX_MOVIES_PER_GENRE; i++) {
-            JSONObject movie = (JSONObject) results.get(i);
-            String title = (String) movie.get("title");
+            for (int i = 0; i < results.size(); i++) {
+                JSONObject movie = (JSONObject) results.get(i);
 
-            JCheckBox checkBox = new JCheckBox(title);
-            selectedMoviesPerGenre.get(action).add(checkBox);
-            movieListPanel.add(checkBox);
-            count++;
+            // Access specific properties of each movie object
+                String title = (String) movie.get("title");
+                JSONArray img = (JSONArray)movie.get("imageurl");
+            // Do something with the movie data
+                System.out.print("Title: " + title);
+                try{
+                System.out.print("   IMG: " + (String)img.get(0));
+                }
+                catch(IndexOutOfBoundsException io){
+                System.out.print("  NO IMAGE PRESENT");
+                }
+                try{
+                    Double rating = (Double)movie.get("imdbrating");
+                    System.out.println("   Rating: " + rating);
+                }
+                catch(Exception e){
+                    long rating = (long)movie.get("imdbrating");
+                    System.out.println("   Rating: " + rating);
+                }
+            
+            }
         }
-
-        // Refresh the UI
-        frame.pack();
+        catch(Exception e){
+            System.out.println("OOPS!!! Error Occured, Have A Look");
+        }
     }
 }
-
