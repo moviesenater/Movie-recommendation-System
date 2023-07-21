8bf1a6832d31e8b8e4c3bf30151350d926cbf839
moviesenater <139667683+moviesenater@users.noreply.github.com>
2023-07-21 17:08:09 +0530

    Update http.java

--- http.java
+++ http.java
@@ -1,63 +1,106 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class http {
-    public static void main(String[] args) throws Exception {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter genre: ");
-        String genre = s.nextLine();
-        System.out.print("Enter language: ");
-        String lang = s.next();
-        s.close();
-        try{
-            HttpRequest request = HttpRequest.newBuilder(new URI("https://ott-details.p.rapidapi.com/advancedsearch?start_year=2020&end_year=2020&min_imdb=6&max_imdb=7.8&genre=" + genre + "&language=" + lang + "&type=movie&sort=latest&page=1"))
-                .header("X-RapidAPI-Key", "65fd17147fmshc51ddabff33471cp1f7ccfjsn59de4e138bd0")
-                .header("X-RapidAPI-Host", "ott-details.p.rapidapi.com")
-                .build();
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(http::createAndShowGUI);
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Movie Search");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        // Create input fields and fetch button
+        JTextField genreField = new JTextField(20);
+        JTextField languageField = new JTextField(20);
+        JButton fetchButton = new JButton("Fetch Movies");
+
+        // Create table to display the movie data
+        DefaultTableModel tableModel = new DefaultTableModel(new Object[]{"Title", "Image URL", "Rating"}, 0);
+        JTable table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        fetchButton.addActionListener(e -> {
+            String genre = genreField.getText();
+            String lang = languageField.getText();
+            fetchMovies(genre, lang, tableModel);
+        });
+
+        // Create input panel
+        JPanel inputPanel = new JPanel();
+        inputPanel.add(new JLabel("Enter genre:"));
+        inputPanel.add(genreField);
+        inputPanel.add(new JLabel("Enter language:"));
+        inputPanel.add(languageField);
+        inputPanel.add(fetchButton);
+
+        // Add components to the frame
+        frame.add(inputPanel, BorderLayout.NORTH);
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void fetchMovies(String genre, String lang, DefaultTableModel tableModel) {
+        try {
+            String apiUrl = "https://ott-details.p.rapidapi.com/advancedsearch?start_year=2020&end_year=2020&min_imdb=6&max_imdb=7.8&genre=" + genre + "&language=" + lang + "&type=movie&sort=latest&page=1";
+
+            HttpRequest request = HttpRequest.newBuilder(new URI(apiUrl))
+                    .header("X-RapidAPI-Key", "0c41303e37msh3ca9c5049b62129p1efd89jsndaaf78084778") // Replace with your API key
+                    .header("X-RapidAPI-Host", "ott-details.p.rapidapi.com")
+                    .build();
             HttpClient client = HttpClient.newHttpClient();
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            String responseBody = response.body(); 
+            String responseBody = response.body();
+            System.out.println(responseBody); // Print the API response in the console
+
             JSONParser parser = new JSONParser();
             JSONObject json = (JSONObject) parser.parse(responseBody);
-
-        // Access the data from the JSON object
             JSONArray results = (JSONArray) json.get("results");
 
-        // Iterate over the results array and access individual objects
-            for (int i = 0; i < results.size(); i++) {
-                JSONObject movie = (JSONObject) results.get(i);
+            // Clear the existing table data
+            tableModel.setRowCount(0);
 
-            // Access specific properties of each movie object
-                String title = (String) movie.get("title");
-                JSONArray img = (JSONArray)movie.get("imageurl");
-            // Do something with the movie data
-                System.out.print("Title: " + title);
-                try{
-                System.out.print("   IMG: " + (String)img.get(0));
+            if (results == null || results.isEmpty()) {
+                // Display a message in the GUI when no movies are found
+                JOptionPane.showMessageDialog(null, "No movies found for the given genre and language.", "No Movies", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                // Iterate over the first 10 results and add them to the table
+                int limit = Math.min(results.size(), 10); // Limit to the first 10 movies
+                for (int i = 0; i < limit; i++) {
+                    JSONObject movie = (JSONObject) results.get(i);
+                    String title = (String) movie.get("title");
+                    JSONArray img = (JSONArray) movie.get("imageurl");
+
+                    Object[] rowData = new Object[3];
+                    rowData[0] = title;
+                    rowData[1] = (img != null && !img.isEmpty()) ? img.get(0) : "No Image";
+
+                    try {
+                        Double rating = (Double) movie.get("imdbrating");
+                        rowData[2] = rating;
+                    } catch (Exception e) {
+                        long rating = (long) movie.get("imdbrating");
+                        rowData[2] = rating;
+                    }
+
+                    tableModel.addRow(rowData);
                 }
-                catch(IndexOutOfBoundsException io){
-                System.out.print("  NO IMAGE PRESENT");
-                }
-                try{
-                    Double rating = (Double)movie.get("imdbrating");
-                    System.out.println("   Rating: " + rating);
-                }
-                catch(Exception e){
-                    long rating = (long)movie.get("imdbrating");
-                    System.out.println("   Rating: " + rating);
-                }
-            
             }
-        }
-        catch(Exception e){
-            System.out.println("OOPS!!! Error Occured, Have A Look");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error occurred: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
     }
 }
